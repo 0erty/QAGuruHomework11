82c38eddac8b245956ce23f24205add77f571d6f
0erty <derty9726@gmail.com>
2021-10-17 15:42:54 +0300

    Update TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -31,7 +31,7 @@ public class TestBase {
         Configuration.startMaximized = true;
         String login = credentials.login();
         String password = credentials.password();
-        String url = System.getProperty("url");
+        String url = System.getProperty("url", "selenoid.autotests.cloud/wd/hub/");
         Configuration.remote = format("https://%s:%s@%s", login, password, url);
     }
 
